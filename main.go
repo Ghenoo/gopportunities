46e73f48b7cac4c70352d4f4f2341974a026ed10
Ghenoo <lucasgheno600@gmail.com>
2024-02-29 11:27:46 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/ghenoo/gopportunities/router"
+import (
+	"github.com/ghenoo/gopportunities/config"
+	"github.com/ghenoo/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initilization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize()
 }
