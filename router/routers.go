afc4f7e018774bf4a31ba5b19e3def39527c4bef
Ghenoo <lucasgheno600@gmail.com>
2024-02-29 10:17:35 -0300

    feat: add routes logic

--- router/routers.go
+++ router/routers.go
@@ -4,36 +4,35 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/twitchyliquid64/golang-asm/obj"
 )
 
 func initializeRoutes(router *gin.Engine) {
-	v1 *gin.RouterGroup := router.Group(relativePath: "/api/v1")
+	v1 := router.Group("/api/v1")
 	{
-		v1.GET(relativePath: "/opening", func(ctx *gin.Context) {	
-			ctx.JSON(http.StatusOK,gin.H{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "GET Opening",
-			 })
-		 })
-		 v1.POST(relativePath: "/opening", func(ctx *gin.Context) {	
-			ctx.JSON(http.StatusOK,gin.H{
+			})
+		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "POST Opening",
-			 })
-		 })
-		 v1.DELETE(relativePath: "/opening", func(ctx *gin.Context) {	
-			ctx.JSON(http.StatusOK,gin.H{
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "DELETE Opening",
-			 })
-		 })
-		 v1.PUT(relativePath: "/opening", func(ctx *gin.Context) {	
-			ctx.JSON(http.StatusOK,gin.H{
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "PUT Opening",
-			 })
-		 })
-		 v1.GET(relativePath: "/openings", func(ctx *gin.Context) {	
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "GET Opening",
-			 })
-		 })
+			})
+		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Openings",
+			})
+		})
 	}
 }
