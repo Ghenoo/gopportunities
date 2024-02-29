4bc1757223d002d4f6074fed62e7e5afbdb188b7
Ghenoo <lucasgheno600@gmail.com>
2024-02-29 09:45:17 -0300

    add router

--- router/router.go
+++ router/router.go
@@ -3,14 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	// Incializa o Router (r) utilizando as configurações Default do gin
+	// Initialize Router
 	Router := gin.Default()
-	// Definindo a rota /ping
-	Router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// Inicia o servidor na porta 8080
+
+	// Initialize Routes
+	InitializeRoutes(Router)
+
+	// Run the server
 	Router.Run("address:8080")
 }
